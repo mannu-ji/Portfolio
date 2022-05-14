87d25af2ce6d7d6bc7303b3f997803f1c67bb917
Manpreet Singh <manpreet10021997@gmail.com>
2022-05-15 04:42:18 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en">
+<head>
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manpreet Singh | Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+
+
+</head>
+<body>
+
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Mannu<span>Ji</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hello, my name is</div>
+                <div class="text-2">Manpreet Singh</div>
+                <div class="text-3">And I'm a <span class="typing"></span></div>
+                <a href="#about">Know me</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/profile-1.jpeg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">I'm Manpreet and I'm a <span class="typing-2"></span></div>
+                    <p>
+Hi, My name is Manpreet Singh. ​I’ve recently done my Masters in Computer Applications(MCA) with Computer Science stream from Babasaheb Bhimrao Ambedkar University with 8.1 CGPA in 2021 and Bachelors in Computer Application(BCA) from Integral University with  8 CGPA in 2018. Also, I did my 12th & 10th from UP Board in the year 2015 and 2013 respectively.I was born and raised in Lucknow,UP.
+ther than that, if I talk about my strengths – I can say that I’m a quick learner, team player, adaptable and creative person
+</p>
+                    <a href="file.pdf">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">My creative skills & experiences.</div>
+                    <p>Coming to my Skills, I have done my training in Coderope Consultancy Pvt. Ltd. in PHP & MySQL. Currently I'm working on Reactjs.
+                      I have created 5 Projects(Online Shopping,Online Examination System,Hospital Mgmt,Attendance..etc) in PHP&MySQL and 2 small Projects(Live Corona,TodoList) in Reactjs as an practice
+
+                    </p>
+                    <a href="https://github.com/mannu-ji" target="_blank">Github</a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>Core PHP</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MySQL</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>React.js</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    <p>
+
+
+                      If you would like to get in touch with me, be it for
+                       exploring a technology, a business, or to just say hi, feel free to send me an email at <b>mannu.portfolio@gmail.com</b>
+                    <br/><br/><b>Feel free to get in touch. Cheers!</b>
+                    </p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Name</div>
+                                <div class="sub-title">Manpreet Singh</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Address</div>
+                                <div class="sub-title">Uttar Pradesh, India</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">manpreet10021997@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Message me</div>
+                    <form method="post" >
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Name" name="name" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Email" name="email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Subject" name="subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Message.." name="message" required></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button type="submit">Send message</button>
+                            <?php
+
+
+                            include('connect.php');
+
+
+                            if((isset($_POST['name']) && !empty($_POST['name']))
+                            && (isset($_POST['email']) && !empty($_POST['email']))
+                            && (isset($_POST['subject']) && !empty($_POST['subject']))){
+                            	$name = $_POST['name'];
+                            	$email = $_POST['email'];
+                            	$subject = $_POST['subject'];
+                            	$message = $_POST['message'];
+
+                            	$to = "mannu.portfolio@gmail.com";
+                            	$headers = "From : " . $email;
+
+                            	if( mail($to, $subject, $message, $headers)){
+                            echo '<script>alert("E-Mail Sent successfully, we will get back to you soon.")</script>';
+                            $query = "INSERT INTO `contact` (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
+                            $result = mysqli_query($connection, $query);
+
+                            	}
+                            }
+
+                             ?>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="https://www.linkedin.com/in/manpreetji/">Manpreet Singh</a> | <span class="far fa-copyright"></span> 2021 All rights reserved.</span>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
